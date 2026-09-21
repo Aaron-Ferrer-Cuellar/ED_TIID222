8f322f667e940b58ce8598b1e2b3f4e76b54f3ab
Aaron-Ferrer-Cuellar <aaronferrercuellar@gmail.com>
2026-09-21 16:38:36 -0600

    Ejercicios de arreglos usando append

--- arreglos.py
+++ arreglos.py
@@ -1,3 +1,5 @@
+""" 
+
 #Declarando un array
 numeros = [10, 20, 30, 40, 50]
 
@@ -28,3 +30,51 @@ frutas.remove("Manzana")
 print(frutas)
 
 
+
+# Declaracion de un arreglo vacio
+Arrglo = [];
+print(Arrglo)
+
+n = int(input("Ingrese el tamano del arreglo: "))
+
+for i in range(n):
+    dato = int(input("Ingrese un numero para el arreglo: "))
+    Arrglo.append(dato)
+
+print(Arrglo)
+
+Arrglo2 = [0] * n
+n2 = int(input("Ingrese el tamano del arreglo: "))
+
+for i in range(n2):
+    dato2 = int(input("Ingrese un numero para el arreglo: "))
+    Arrglo2[i] = dato
+
+print(Arrglo2)
+
+"""
+
+
+Arrglo = []
+Arreglo2 = []
+
+n = int(input("Ingrese el tamano del arreglo: "))
+
+for i in range(n):
+    dato = int(input("Ingrese un numero para el arreglo: "))
+    Arrglo.append(dato)
+
+for i in range(n):
+    num = Arrglo[i]
+    if num % 5 == 0:
+        Arreglo2.append(num)
+    else:
+        Arreglo2.append(num + (5 - (num % 5)))
+
+print("Array original:", *Arrglo)
+print("Array cincuerizado:", *Arreglo2)
+
+numw = 23
+print(numw % 5)
+
+    
